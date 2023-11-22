56cb8321894a8b2de1b240b78a933afeede28db9
ShawarmaExpress <jose.rafael.molina.montano@gmail.com>
2023-11-22 01:50:36 -0500

    construccion de la app basica terminada

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -31,7 +31,7 @@ export const HomeContainer = () => {
         </div>
       </div>
       <InfoAlert
-        infoMessage="Bienvenido a esta prueba, podras hacer el CRUD de usuarios, pero ten en cuenta que no podras hacer nada a los usuarios Jhon, Maria o Admin, por restricciones de la API de Platzi"
+        infoMessage="Bienvenido a esta prueba, podras hacer el CRUD de usuarios, pero ten en cuenta que no podras hacer nada a los usuarios Jhon, Maria o Admin, por restricciones de la API de Platzi. Tambien recuerda que esta API es libre, por lo que los datos aqui mostrados tambien son de otras personas, y la API se limia cada cierto tiempo por temas de mantenimiento, asi que no te asustes si los usuarios desaparecen."
       />
 
       <UsersGridList
